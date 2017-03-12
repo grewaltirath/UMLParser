8fe53a8700d504c25a3f3d7ab96e59b03916ea2e
grewaltirath <tirathsingh.grewal@sjsu.edu>
2017-03-11 18:42:16 -0800

    Made a new file ParseEngine.java
    9th commit

---
+++ Source/ParseEngine.java
@@ -0,0 +1,28 @@
+import java.io.*;
+import java.util.*;
+import java.lang.*;
+
+
+//importing javaparser library
+import com.github.javaparser.*;
+import com.github.javaparser.ast.*;
+import com.github.javaparser.ast.body.*;
+import com.github.javaparser.ast.type.ClassOrInterfaceType;
+
+public class ParseEngine {
+    final String incomingPath;
+    final String outgoingPath;
+    HashMap<String, Boolean> map;
+    HashMap<String, String> mapClassConn;
+    String yumlCode;
+    ArrayList<CompilationUnit> cuArray;
+
+//Initializing the constructor
+    ParseEngine(String incomingPath, String outgoingFile) {
+        this.incomingPath = incomingPath;
+        this.outgoingPath = incomingPath + "\\" + outgoingFile + ".png";
+        map = new HashMap<String, Boolean>();
+        mapClassConn = new HashMap<String, String>();
+        yumlCode = "";
+    }
+    }
