26c4a04ce617fc35504708a52abae48a9e1e26ad
grewaltirath <tirathsingh.grewal@sjsu.edu>
2017-04-09 21:27:39 -0700

    Completed the draw() method
    Using yUML to generate diagram

--- Source/Draw.java
+++ Source/Draw.java
@@ -7,5 +7,34 @@ public class Draw {
 
     public static Boolean image(String content, String dir) {
 
-      
+        try {
+            String ref = "http://yuml.me/diagram/plain/class/" + content
+                    + ".png"; //link to generate diagram
+            URL u = new URL(ref);
+            HttpURLConnection h = (HttpURLConnection) u.openConnection();
+            h.setRequestMethod("GET"); //making a HTTP GET Request
+            h.setRequestProperty("Accept", "application/json");
+
+            if (h.getResponseCode() != 200) {
+                throw new RuntimeException(
+                        "HTTP Response Code: " + h.getResponseCode());
+            }
+            OutputStream op = new FileOutputStream(new File(dir));
+            int read = 0;
+            byte[] b = new byte[1024];
+
+            while ((read = h.getInputStream().read(b)) != -1) {
+                op.write(b, 0, read);
+            }
+            op.close();
+            h.disconnect();
+        } 
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+        catch (MalformedURLException e) {
+            e.printStackTrace();
+        } 
+        return null;
+    }
 }
