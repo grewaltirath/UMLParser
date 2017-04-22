4133d0ef354cc0afdd742220c7b2e0b1f240e584
grewaltirath <tirathsingh.grewal@sjsu.edu>
2017-04-21 19:14:36 -0700

    Written the constructor for SequenceParser
    Constructor

--- Source/SequenceParser.java
+++ Source/SequenceParser.java
@@ -12,7 +12,29 @@ import com.github.javaparser.ast.stmt.*;
 import net.sourceforge.plantuml.SourceStringReader;
 
 public class SequenceParser {
-    
+    HashMap<String, String> classMap;
+    ArrayList<CompilationUnit> c;
+    HashMap<String, ArrayList<MethodCallExpr>> methodMap;
+    final String incoming;
+    final String outgoing;
+    final String function;
+    final String claassName;
+    String s;
+
+//constructor for SequenceParser
+    SequenceParser(String incoming, String claassName, String function,
+            String fileName) {
+        this.incoming = incoming;
+        this.outgoing = incoming + "\\" + fileName + ".png";
+        this.claassName = claassName;
+        this.function = function;
+        classMap = new HashMap<String, String>();
+        methodMap = new HashMap<String, ArrayList<MethodCallExpr>>();
+        s = "@startuml\n";
+    }
+
+
+
    
 
 }
