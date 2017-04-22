54298498370d1375d522f2e5cc405ac4763d6894
grewaltirath <tirathsingh.grewal@sjsu.edu>
2017-04-21 19:13:09 -0700

    Started with Sequence diagram
    Started sequence

--- Source/SequenceParser.java
+++ Source/SequenceParser.java
@@ -1,37 +1,18 @@
+package umlparser.umlparser;
+
 import java.io.*;
 import java.util.*;
 
-
-//importing the javaparser library
 import com.github.javaparser.*;
 import com.github.javaparser.ast.*;
 import com.github.javaparser.ast.body.*;
 import com.github.javaparser.ast.expr.MethodCallExpr;
 import com.github.javaparser.ast.stmt.*;
 
-public class SequenceParser{
+import net.sourceforge.plantuml.SourceStringReader;
 
-    final String incomingPath;
-    final String outgoingPath;
-    final String incomingFunctionName;
-    final String incomingClassName;
-    String pumlCode;
+public class SequenceParser {
+    
+   
 
-    HashMap<String, String> mapMethodClass;
-    ArrayList<CompilationUnit> cuArray;
-    HashMap<String, ArrayList<MethodCallExpr>> mapMethodCalls;
-
-
-//Initialize the constructor
-
-    SequenceParser(String incomingPath, String incomingClassName, String incomingFunctionName,
-            String outgoingFile) {
-        this.incomingPath = inPath;
-        this.outgoingPath = inPath + "\\" + outFile + ".png";
-        this.incomingClassName = incomingClassName;
-        this.incomingFunctionName = incomingFunctionName;
-        mapMethodClass = new HashMap<String, String>();
-        mapMethodCalls = new HashMap<String, ArrayList<MethodCallExpr>>();
-        pumlCode = "@startuml\n";
-    }
 }
